016d0a95a737f352c70b99a8ad9bb4538866842b
leon-carvalho <leonardoscc15@gmail.com>
2020-11-01 11:47:43 -0300

    feat: add global styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,12 @@
+import GlobalStyle from "../styles/GlobalStyle";
+
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default MyApp;
